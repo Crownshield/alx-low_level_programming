368cfda6af5710569d79bbe26adcd2d6e8dc5517
Crownshield <adefisayoadeniji@gmail.com>
2022-07-01 15:12:48 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	i = 48;
+	j = 48;
+	while  ((i < 58) && (j < 58))
+	{
+		putchar(i);
+		putchar(j);
+		if ((i == 57) && (j == 57))
+		{
+			putchar('\n');
+			i++;
+			j++;
+		}
+		else
+		{
+			putchar(44);
+			putchar(32);
+			if (j < 57)
+			{
+				j++;
+			}
+			else
+			{
+				j = 48;
+				i++;
+			}
+		}
+
+	}
+
+	return (0);
+}
