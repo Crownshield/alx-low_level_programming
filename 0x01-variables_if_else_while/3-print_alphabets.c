fb840285bee3adc422363fceab14c2894235f4d5
Crownshield <adefisayoadeniji@gmail.com>
2022-07-01 14:05:11 -0700

    3 alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,10 +11,8 @@ int main(void)
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-	for (low = 'A'; low <= 'Z'; low++)
-		putchar(low);
-		putchar('\n');
+		putchar(low)
+			putchar('\n');
 
 	return (0);
 }
