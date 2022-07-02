c4cf5ef55536d746c829641701c5988cc67e7428
Crownshield <adefisayoadeniji@gmail.com>
2022-07-01 17:52:43 -0700

    100 print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
-
+#include <time.h>
+#include <stdlib.h>
 /**
- *main - print 00 to 99 with no duplicate digits or combos: no 11, no 10 (01)
+ * main - Entry point
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
-
 int main(void)
 {
-  int ones;
-  int tens;
+	int ones;
+	int tens;
 
-  for (tens = '0'; tens <= '9'; tens++) /*increment tens*/
-  {
-    for (ones = (tens + 1); ones <= '9'; ones++) /*one's ten+1*/
-    {
-      putchar(tens);
-      putchar(ones);
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for (ones = (tens + 1); ones <= '9'; ones++) /* ones ten+1*/
+		{
+			putchar(tens);
+			putchar(ones);
 
-      if (tens != '8' || ones != '9') /*print commas*/
-      {
-        putchar(',');
-        putchar(' ');
-      }
-    }
-  }
-  putchar('\n');
-
-  return (0);
+			if (tens != '8' || ones != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
