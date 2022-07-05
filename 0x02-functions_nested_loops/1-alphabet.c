1108ae6f9ac22f2e5aa312a8d5ee91c779191501
Crownshield <adefisayoadeniji@gmail.com>
2022-07-05 07:55:03 -0700

    main header file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * print_alphabet - utilizes _putchar function to print alphabet
+ *           a - z in lower case
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		_putchar(ch);
+	_putchar('\n');
+}
