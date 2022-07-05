1ef0f8cf7d2e7615a8bfba4eed22440c4e081f34
Crownshield <adefisayoadeniji@gmail.com>
2022-07-05 08:12:15 -0700

    1 Alphabet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,11 @@
-#include "main.h"
-
+#include"main.h"
 /**
  * main - Entry point
  *
  * Description: prints _putchar using putchar prototype
  *
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char str[] = "_putchar";
@@ -16,6 +14,5 @@ int main(void)
 	for (ch = 0; ch < 8; ++ch)
 		_putchar(str[ch]);
 	_putchar('\n');
-
 	return (0);
 }
